a85caac80fa8cd7589c3b8c7c1f8ba7b04d1e25c
Anton Irvold <40317988+antonvdi@users.noreply.github.com>
2020-09-07 20:01:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # cd C:\Users\anton_mc03yx6\Documents\GitHub\map-maker
 #name generation (archipelago, peninsula, strait, etc)
 #auto generate terrain (perlin noise)
-import pygame, os
+#1. improve render code
+import pygame, os, noise
+from random import randint
 
 pygame.init()
 width 		= 1280
@@ -16,6 +18,7 @@ class World:
 		self.current = "land"
 
 	def render(self):
+		#iterate through the map
 		for x in range(0,len(self.chart)):
 			for y in range(0,len(self.chart[x])):
 				screen.blit(image_dict["water"], (x*resolution, y*resolution))
@@ -30,71 +33,105 @@ class World:
 						beaches += 1
 					if self.chart[x][y+1] == "water":
 						beaches += 1
-					
+
 					#find out how to rotate the tile and print it on the screen
 					if beaches == 0:
 						#center
-						screen.blit(image_dict["%s_center" % self.chart[x][y]], (x*resolution-20, y*resolution-20))
+						screen.blit(image_dict["%s_center" % self.chart[x][y]], 
+									(x*resolution-20, y*resolution-20))
 
 					elif beaches == 1:
 						#sides
 						if self.chart[x-1][y] == "water":
-							screen.blit(image_dict["%s_side" % self.chart[x][y]], (x*resolution-20, y*resolution-20))
+							screen.blit(image_dict["%s_side" % self.chart[x][y]], 
+										(x*resolution-20, y*resolution-20))
 						elif self.chart[x+1][y] == "water":
-							screen.blit(rot_center(image_dict["%s_side" % self.chart[x][y]], 180), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_side" % self.chart[x][y]], 180), 
+										(x*resolution-20, y*resolution-20))
 						elif self.chart[x][y-1] == "water":
-							screen.blit(rot_center(image_dict["%s_side" % self.chart[x][y]], -90), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_side" % self.chart[x][y]], -90), 
+										(x*resolution-20, y*resolution-20))
 						elif self.chart[x][y+1] == "water":
-							screen.blit(rot_center(image_dict["%s_side" % self.chart[x][y]], 90), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_side" % self.chart[x][y]], 90), 
+										(x*resolution-20, y*resolution-20))
 
 					elif beaches == 2:
 						#corners
 						if (self.chart[x-1][y] == "water") and (self.chart[x][y-1] == "water"):
-							screen.blit(image_dict["%s_corner" % self.chart[x][y]], (x*resolution-20, y*resolution-20))
+							screen.blit(image_dict["%s_corner" % self.chart[x][y]], 
+										(x*resolution-20, y*resolution-20))
 						elif (self.chart[x][y-1] == "water") and (self.chart[x+1][y] == "water"):
-							screen.blit(rot_center(image_dict["%s_corner" % self.chart[x][y]], -90), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_corner" % self.chart[x][y]], -90), 
+										(x*resolution-20, y*resolution-20))
 						elif (self.chart[x+1][y] == "water") and (self.chart[x][y+1] == "water"):
-							screen.blit(rot_center(image_dict["%s_corner" % self.chart[x][y]], 180), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_corner" % self.chart[x][y]], 180), 
+										(x*resolution-20, y*resolution-20))
 						elif (self.chart[x-1][y] == "water") and (self.chart[x][y+1] == "water"):
-							screen.blit(rot_center(image_dict["%s_corner" % self.chart[x][y]], 90), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_corner" % self.chart[x][y]], 90), 
+										(x*resolution-20, y*resolution-20))
 						#bridges
 						elif (self.chart[x-1][y] == "water") and (self.chart[x+1][y] == "water"):
-							screen.blit(image_dict["%s_bridge" % self.chart[x][y]], (x*resolution-20, y*resolution-20))
+							screen.blit(image_dict["%s_bridge" % self.chart[x][y]], 
+										(x*resolution-20, y*resolution-20))
 						elif (self.chart[x][y-1] == "water") and (self.chart[x][y+1] == "water"):
-							screen.blit(rot_center(image_dict["%s_bridge" % self.chart[x][y]], 90), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_bridge" % self.chart[x][y]], 90), 
+										(x*resolution-20, y*resolution-20))
 
 					elif beaches == 3:
 						if self.chart[x][y+1] != "water":
-							screen.blit(image_dict["%s_peninsula" % self.chart[x][y]], (x*resolution-20, y*resolution-20))
+							screen.blit(image_dict["%s_peninsula" % self.chart[x][y]], 
+										(x*resolution-20, y*resolution-20))
 						elif self.chart[x+1][y] != "water":
-							screen.blit(rot_center(image_dict["%s_peninsula" % self.chart[x][y]], 90), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_peninsula" % self.chart[x][y]], 90), 
+										(x*resolution-20, y*resolution-20))
 						elif self.chart[x][y-1] != "water":
-							screen.blit(rot_center(image_dict["%s_peninsula" % self.chart[x][y]], 180), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_peninsula" % self.chart[x][y]], 180), 
+										(x*resolution-20, y*resolution-20))
 						elif self.chart[x-1][y] != "water":
-							screen.blit(rot_center(image_dict["%s_peninsula" % self.chart[x][y]], 270), (x*resolution-20, y*resolution-20))
+							screen.blit(rot_center(image_dict["%s_peninsula" % self.chart[x][y]], 270), 
+										(x*resolution-20, y*resolution-20))
 					elif beaches == 4:
 						#islands
-						screen.blit(image_dict["%s_island" % self.chart[x][y]], (x*resolution-20, y*resolution-20))
+						screen.blit(image_dict["%s_island" % self.chart[x][y]], 
+									(x*resolution-20, y*resolution-20))
 
 
 	def click(self, pos_x, pos_y):
+		#populates the clicked position in the chart with the current land type
 		self.chart[int(pos_x/resolution)][int(pos_y/resolution)] = self.current
 
 	def rightClick(self, pos_x, pos_y):
+		#populates the clicked position in the chart with water
 		self.chart[int(pos_x/resolution)][int(pos_y/resolution)] = "water"
 
 	def desertClick(self):
+		#shuffles between the land types
 		if self.current == "land":
 			self.current = "desert"
 		elif self.current == "desert":
 			self.current = "snow"
 		elif self.current == "snow":
 			self.current = "land"
+	def noise(self, scale, octaves, persistence, lacunarity):
+		self.noisechart = [[0 for y in range(int(height/resolution+1))] for x in range(int(width/resolution+1))]
+		for x in range(0,len(self.chart)):
+			for y in range(0,len(self.chart[x])):
+				self.noisechart[x][y] = noise.pnoise2(	x/scale,
+														y/scale,
+														octaves = octaves,
+														persistence = persistence,
+														lacunarity = lacunarity,
+														repeatx = int(width/resolution+1),
+														repeaty = int(height/resolution+1),
+														base = randint(0,100))
+				if self.noisechart[x][y] > 0.08:
+					self.chart[x][y] = "land"
 
 def main():
 	done = False
 	#create the world/map-instance
 	chartInstance = World()
+	#render the map
 	chartInstance.render()
 
 	while not done:
@@ -104,15 +141,21 @@ def main():
 				done = True
 			#handle mos click pos
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				pos_x, pos_y = pygame.mouse.get_pos()
-				if event.button == 1:
-					chartInstance.click(pos_x, pos_y)
-				elif event.button == 3:
-					chartInstance.rightClick(pos_x, pos_y)
+					pos_x, pos_y = pygame.mouse.get_pos()
+					#if left mouse button is pressed, create land
+					if event.button == 1:
+						chartInstance.click(pos_x, pos_y)
+					#if right mousebutton is pressed, remove land
+					elif event.button == 3:
+						chartInstance.rightClick(pos_x, pos_y)
+			#if space is clicked, shuffle current type
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE:
 					chartInstance.desertClick()
+				if event.key == pygame.K_p:
+					chartInstance.noise(100, 6, 0.5, 2.0)
 
+		#render map and update screen
 		chartInstance.render()
 		pygame.display.update()
 
@@ -135,5 +178,6 @@ def load_images(path_to_directory):
 			image_dict[key] = pygame.image.load(path).convert_alpha()
 	return image_dict
 
+#load sprites
 image_dict = load_images('sprites')
 main()
